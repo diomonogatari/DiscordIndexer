79e8f193489d64d5ffd0392068bb9127af002cc9
Diomonogatari <diodude.lol@gmail.com>
2022-01-03 22:03:52 +0000

    improve save by making it more generic

--- SaveImage.py
+++ SaveImage.py
@@ -1,20 +1,32 @@
 from discord.ext import commands
+from dotenv import load_dotenv
+import os
 import uuid
-import requests
-import shutil
+import Utils
+
+#load the env that has the confidential tokens/keys
+load_dotenv()
+storingPath = "storage"
 
 
 client = commands.Bot(command_prefix='.')
 
-
+# something my bot will be listening to
 @client.event
 async def on_ready():
-    print("Bot is ready")
+    print("I'm now online!")
+
 
 # I'm defining a command that will be named "save" that the bot recognizes 
 @client.command()
 async def save(context):
-    imageName = str(uuid.uuid4()) + '.jpg'
-    await context.message.attachments[0].save(imageName)
+    try:
+        extension = Utils.getMediaType(context.message.attachments[0].content_type)
+        filename = str(uuid.uuid4()) + extension
+    except ValueError as e:
+        context.message.channel.send(e)
 
-client.run('TOKEN')
+    completeName = os.path.join(storingPath, filename)
+    await context.message.attachments[0].save(completeName)
+
+client.run(os.environ.get('secretToken'))
---
+++ Utils.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+def getMediaType(argument):
+    switcher = {
+        "image/jpeg": ".jpg",
+        "image/png": ".png",
+        "image/svg+xml": ".svg",
+        "application/pdf": ".pdf",
+        "video/webm": ".webm",
+        "video/mp4": ".mp4",
+    }
+    result = switcher.get(argument, "nothing")
+    if(result == "nothing"):
+        raise ValueError("The given file format for this media is not compatible")
+    return result
